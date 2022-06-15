6c9845b8591ab96c8ada984bfa3be02a736c87c2
Fraser Love <fraser.love@mre.medical.canon>
2022-06-15 09:12:37 +0100

    Added .raw file for testing

--- backend/main.ts
+++ backend/main.ts
@@ -4,7 +4,7 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 
-var volume = new volume.Volume('Volume_ce4cdea10ce4bb0d2d697e32b05dfa0e');
+var volume = new volume.Volume('Volume_ce879dc4b13afbf761f38b8ccd8ff764');
 var app = express();
 
 app.use(express.static(path.resolve(__dirname, '../../frontend/dist')));
