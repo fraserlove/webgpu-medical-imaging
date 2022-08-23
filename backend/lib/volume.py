490c8d85b56f4a632569435a358d4e440db0c1e3
fraserlove <me@fraser.love>
2022-08-23 15:58:10 +0100

    Implemented loading of dicom files

--- backend/lib/volume.py
+++ backend/lib/volume.py
@@ -1,13 +1,33 @@
 class Volume:
 
-    def __init__(self, meta):
-        self.size = [int(meta['Width']), int(meta['Height']),int(meta['Image_count'])]
-        self.bitsPerVoxel = int(meta['Bits_per_voxel'])
-        self.bytesPerLine = int(meta['Bytes_per_line'])
-        self.format = meta['Pixel_Format']
-        self.filename = meta['Filename']
-        self.boundingBox = [ float(i) for i in meta['Bounding_box'][1:-1].split(',') ][:3]
+    def __init__(self, size, bitsPerVoxel, bytesPerLine, signed, filename, boundingBox):
+        self.size = size
+        self.bitsPerVoxel = bitsPerVoxel
+        self.bytesPerLine = bytesPerLine
+        self.signed = signed
+        self.filename = filename
+        self.boundingBox = boundingBox
         self.find_format()
+
+    @classmethod
+    def from_xml(cls, meta):
+        size = [int(meta['Width']), int(meta['Height']), int(meta['Image_count'])]
+        bitsPerVoxel = int(meta['Bits_per_voxel'])
+        bytesPerLine = int(meta['Bytes_per_line'])
+        signed = 1 if meta['Pixel_Format'][-1] == 's' else 0
+        filename = meta['Filename']
+        boundingBox = [ float(i) for i in meta['Bounding_box'][1:-1].split(',') ][:3]
+        return cls(size, bitsPerVoxel, bytesPerLine, signed, filename, boundingBox)
+
+    @classmethod
+    def from_dicom(cls, meta, depth, filename):
+        print(meta)
+        size = [int(meta.Rows), int(meta.Columns), depth]
+        bitsPerVoxel = meta.BitsAllocated
+        bytesPerLine = size[0] * (bitsPerVoxel // 8)
+        signed = meta.PixelRepresentation
+        boundingBox = [size[0] - 1, size[1] - 1, size[2]] # * meta.PixelSpacing[0]]
+        return cls(size, bitsPerVoxel, bytesPerLine, signed, filename, boundingBox)
 
     def find_format(self):
         if self.bitsPerVoxel == 8: self.textureFormat = 'r8unorm'
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-import os, sys, xmltodict, json, struct
+import os, sys, xmltodict, json, struct, pydicom, numpy, gdcm
 from flask import Flask, render_template, Response, stream_with_context
 from lib.volume import Volume
 from lib.transfer_function import TransferFunction
@@ -14,13 +14,20 @@ class Server():
         self.read_resources()
 
     def read_resources(self):
-        filenames = [f for f in os.listdir(self.res_path) if os.path.isfile(os.path.join(self.res_path, f))]
+        filenames = [f for f in os.listdir(self.res_path)]
         for filename in filenames:
             if filename.split('.')[-1] == 'xml':
                 file = os.path.join(self.res_path, filename)
                 meta = xmltodict.parse(open(file).read())
-                if 'Volume_View' in meta: self.volumes.append(Volume(meta['Volume_View']))
+                if 'Volume_View' in meta: self.volumes.append(Volume.from_xml(meta['Volume_View']))
                 elif 'Transfer_Function' in meta: self.transfer_functions.append(TransferFunction(meta['Transfer_Function']))
+
+            if os.path.isdir(os.path.join(self.res_path, filename)):
+                dcm_files = [f for f in os.listdir(os.path.join(self.res_path, filename)) if f.split('.')[-1] == 'dcm']
+                if len(dcm_files) > 0:
+                    dcm_file = os.path.join(self.res_path, filename, dcm_files[0])
+                    self.volumes.append(Volume.from_dicom(pydicom.read_file(dcm_file), len(dcm_files), filename))
+
         self.setup();
         self.start();
 
@@ -31,42 +38,49 @@ class Server():
 
         @self.app.route('/volumes')
         def volumes():
-            return json.dumps([volume.__dict__ for volume in self.volumes])
+            return json.dumps([vars(volume) for volume in self.volumes])
 
         @self.app.route('/transfer_functions')
         def transfer_functions():
-            return json.dumps([transfer_function.__dict__ for transfer_function in self.transfer_functions])
+            return json.dumps([vars(transfer_function) for transfer_function in self.transfer_functions])
 
         @self.app.route('/volume/<string:filename>')
         def send_volume(filename = None):
             for volume in self.volumes:
                 if filename == volume.filename:
-                    return Response(stream_with_context(self.chunk_file(os.path.join(self.res_path, filename), volume.format)))
+                    if os.path.isdir(os.path.join(self.res_path, filename)): return Response(stream_with_context(self.chunk_dicom(os.path.join(self.res_path, filename))))
+                    else: return Response(stream_with_context(self.chunk_file(os.path.join(self.res_path, filename), volume.signed, volume.bitsPerVoxel)))
 
         @self.app.route('/transfer_function/<string:filename>')
         def send_transfer_function(filename = None):
             if filename in [transfer_function.filename for transfer_function in self.transfer_functions]:
-                return Response(stream_with_context(self.chunk_file(os.path.join(self.res_path, filename), '')))
+                return Response(stream_with_context(self.chunk_file(os.path.join(self.res_path, filename), False, None)))
 
     def start(self):
         self.app.run(host='0.0.0.0', port=self.port)
         print('Server started at http://localhost:' + self.port)
 
-    def chunk_file(self, path, format):
+    def chunk_dicom(self, path):
+        for file in sorted([f for f in os.listdir(path) if f.split('.')[-1] == 'dcm']):
+            meta = pydicom.read_file(os.path.join(path, file))
+            rescaleSlope = 1 if meta.get('RescaleSlope') == None else meta.RescaleSlope
+            rescaleIntercept = 0 if meta.get('RescaleIntercept') == None else meta.RescaleIntercept
+            out = (rescaleSlope * meta.pixel_array + rescaleIntercept) + 2 ** 15 - rescaleIntercept
+            yield out.astype(numpy.uint16).tobytes()
+
+    def chunk_file(self, path, signed, bitsPerVoxel):
         CHUNK_SIZE = 2 ** 24
         with open(path, 'rb') as file:
             while True:
                 binary = file.read(CHUNK_SIZE)
-                if format[-1] == 's':
-                    if format[4:6] == '16': format_char = 'h'
-                    elif format[4:6] == '8': format_char = 'b'
+                if signed:
+                    if bitsPerVoxel == 16: format_char = 'h'
+                    elif bitsPerVoxel == 8: format_char = 'b'
                     signed = struct.unpack('{}{}'.format(len(binary) // 2, format_char), binary)
                     unsigned = [short + 2 ** 15 for short in signed]
-                    binary = struct.pack('{}H'.format(len(binary) // 2, format_char.upper()), *unsigned)
-                if binary:
-                    yield binary
-                else:
-                    break
+                    binary = struct.pack('{}{}'.format(len(binary) // 2, format_char.upper()), *unsigned)
+                if binary: yield binary
+                else: break
 
 if __name__ == '__main__':
     server = Server();
